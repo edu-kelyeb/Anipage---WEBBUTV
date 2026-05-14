dca9ee5c31b80c57cdec28d55c7734262be3cfc3
edu-kelyeb <kelvin.yeboah@skola.taby.se>
2026-05-14 23:57:28 +0200

    jag gjort dark mode ser bra ut men kan vara mycket bättre.

--- java/lan.js
+++ java/lan.js
@@ -9,3 +9,10 @@ function switchLanguage(lang) {
   });
   document.documentElement.lang = lang;
 }
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
